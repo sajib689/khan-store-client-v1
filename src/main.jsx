9d40b027a83b9b2d03c1b9434adb7b2df633af1c
sajib689 <jibonraja11@gmail.com>
2024-07-13 11:24:10 +0600

    add navbar logo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Router.jsx";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="font-montFont">
+    <RouterProvider router={router}>
     <App />
-  </div>,
-)
+    </RouterProvider>
+   
+  </div>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+     fontFamily: {
+      montFont: 'font-family: "Montserrat", sans-serif'
+     }
+    },
   },
   plugins: [
     require('daisyui'),
