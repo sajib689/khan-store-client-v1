415c0390e7093c6275588a9e4addefd1cadc88c6
sajib689 <jibonraja11@gmail.com>
2024-07-14 22:51:53 +0600

    add the card

--- src/Components/PopularCategoryCard.jsx
+++ src/Components/PopularCategoryCard.jsx
@@ -1,9 +1,15 @@
 
 
-const PopularCategoryCard = () => {
+const PopularCategoryCard = ({categoryy}) => {
+    const {category,img} = categoryy
     return (
-        <div>
-            
+        <div className="cursor-pointer flex hover:p-7 transition-all duration-25 p-5 flex-col justify-center items-center">
+           <div className="relative opacity-75">
+           <img className="rounded-lg w-48 h-48" src={img} alt="" />
+           </div>
+            <div className="absolute top-0">
+            <h1 className="text-xl bg-cyan-300 rounded-lg p-1">{category}</h1>
+            </div>
         </div>
     );
 };
