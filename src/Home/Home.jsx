3c1132b9a46613c3673f2e75c23cce71cf62d94d
sajib689 <jibonraja11@gmail.com>
2024-07-14 17:30:53 +0600

    add public api

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import PopularProduct from "./PopularProduct";
 
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <PopularProduct/>
         </div>
     );
 };
